0dca4df0dc4f14fc844c7934a9ba46972e4389aa
Maxim Schepelin <m.schepelin@gmail.com>
2016-02-22 20:00:32 +0300

    add tests for some strings methods

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -1,1 +1,29 @@
-const str = 'hello world'
+
+/*
+write a function's body that concat all passed strings to one and returns it
+*/
+export function concat(...strings) {
+    // your code here
+    return ''
+}
+
+
+/*
+write a function's body that returns string
+that contains element of initial strings with odd indexes
+'abcd' -> 'ac'
+'' -> ''
+*/
+export function oddElements(string) {
+    return ''
+}
+
+/*
+write a function's body that returns words count in passed string
+'abcd' -> 1
+'' -> 0
+'foo bar' -> 2
+*/
+export function wordsCount(string) {
+    return 0
+}
