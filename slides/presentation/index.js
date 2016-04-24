d63645175f2e4dee060eedfb4a870f73235f0292
Maxim Schepelin <m.schepelin@gmail.com>
2016-04-24 20:26:40 +0300

    start working on slides

--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -3,34 +3,21 @@ import React from "react"
 
 // Import Spectacle Core tags
 import {
-  Appear,
-  BlockQuote,
-  Cite,
-  CodePane,
   Deck,
-  Fill,
-  Heading,
-  Image,
-  Layout,
-  Link,
-  ListItem,
-  List,
-  Markdown,
-  Quote,
-  Slide,
   Spectacle,
-  Text
+  Slide,
+  Heading,
+  Link,
+  Text,
+  Image
 } from "spectacle"
 
 import CodeSlide from "spectacle-code-slide"
-// Import image preloader util
-import preloader from "spectacle/lib/utils/preloader"
+
+import preloader from "spectacle/lib/utils/preloader";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default"
-
-// Import custom component
-import Interactive from "../assets/interactive"
 
 // Require CSS
 require("normalize.css")
@@ -38,140 +25,55 @@ require("spectacle/lib/themes/default/index.css")
 
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  eich: require("../assets/Brendan_Eich_Mozilla_Foundation_official_photo.jpg")
 }
 
 preloader(images)
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "#e8e40c",
+  black: "#000000",
+  quartenary: "#122b45"
 })
+
+const code = {
+  closure: require("raw!../assets/closure.example")
+}
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Spectacle theme={theme}>
-        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <CodeSlide transition={[]}
-            lang="jsx"
-            code={require("raw!../assets/deck.example")}
-            ranges={[
-              { loc: [0, 28], title: "Walking through some code" },
-              { loc: [0, 1], title: "return statement" },
-              { loc: [1, 2] },
-              { loc: [1, 2], note: "Heres a note!" },
-              { loc: [2, 3] },
-              { loc: [4, 7] },
-              { loc: [8, 10] }
-            ]}
-          />
-          <Slide transition={["zoom"]} bgColor="primary">
+        <Deck transition={["zoom", "slide"]} transitionDuration={500} progress="bar">
+          <Slide transition={[]} bgColor="primary">
             <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+              JavaScript
             </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
+            <Heading size={1} caps textColor="black">
+              Language basics
             </Heading>
             <Link href="https://github.com/FormidableLabs/spectacle">
               <Text bold caps textColor="tertiary">View on Github</Text>
             </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
 
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
+          <Slide transition={[]} bgColor="black">
+            <Image src={images.eich.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Text bold caps textColor="tertiary">Was invented by Brendan Eich in 1995</Text>
+            <Text bold textColor="tertiary">It was designed to consolidate all the things like images styles java applets etc</Text>
           </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
-          </Slide>
+
+          <CodeSlide
+            transition={["slide"]}
+            lang="js"
+            code={code.closure}
+            ranges={[
+              { loc: [0, 6], title: "Closure" },
+              { loc: [1, 5], note: "Function body contains another function definition" },
+              { loc: [2, 3], note: "It is possible to use outer scope variables here" },
+              { loc: [7, 10], note: "How it works" },
+              { loc: [11, 15], note: "Closure used here to keep a value of counter" },
+            ]}/>
         </Deck>
       </Spectacle>
     )
---
+++ slides/presentation/plan.md
@@ -0,0 +1,45 @@
+# JS course plan
+
+## Intro
+
+Today we are going to talk about javascript language basics Thera two parts in our schedule for today. First I will tell you about language show some expamples and basic things. And, in second part you will try to solve some exercises I prepared for you
+
+## Ground rules
+
+- Swich phones to the silent mode
+- Put your hand on if you want ask something
+
+## A bit of history
+
+JS was invented in 1995 by Brendan Eich as a one tool to manage all things in browser like images styles and java applets& Later it was standartized by ECMA as ECMA-262
+
+There are a lot of hype about javascript If you google for `why javascript is` you'll get something like this
+
+- Why javascript is a bad language
+- Why javascript is the best language
+
+We had been had a lot of hype about javascript for a long time. A Last year new language spec has released. And now we have a shiny new language and working spec update process
+
+## Spec update policy
+
+strawman, proposal, draft, candidate, finished
+
+TC39 working process
+
+this process allow use future features right now and allow us don't care about which environment code will work.
+
+## Language basics
+
+JS is a high-level, dynamic, untyped, and interpreted programming language
+
+## Variables
+
+## Data types
+
+## Comparation
+
+## Conditions and loops
+
+## Functons
+
+## Closures
